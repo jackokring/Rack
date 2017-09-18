24ccfb1629678e0c3fdfdfb1dd6eb0bade05b53a
Simon Jackson <jackokring@gmail.com>
2017-09-18 15:45:08 +0100

    and

---
+++ plugins/KRT Free/src/libs.hpp
@@ -0,0 +1,7 @@
+//Include all your libraries here
+//If it is .hpp it will be included maybe use #pragma once
+//If it is .cpp or .hpp (with running code definition, not just outlines and structures)
+//it may cause problems if used more than once.
+//.cpp is made seperate, .hpp is just templates.
+#pragma once
+#include "fmath.hpp"
